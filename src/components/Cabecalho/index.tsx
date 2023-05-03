209576a415e855d28fe0114f86e03196da568a94
EvertonCandelone <everton.lcs.pro@gmail.com>
2023-05-03 18:25:28 -0300

    styled-components

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+const Header = styled.header`
+  background-color: var(--cor-secundaria);
+  color: var(--cor-principal);
+  text-align: center;
+  padding: 24px 0;
+`
+
+function Cabecalho() {
+  return (
+    <Header>
+      <h1>EBAC Jobs</h1>
+    </Header>
+  )
+}
+
+export default Cabecalho
